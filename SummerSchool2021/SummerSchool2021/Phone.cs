8366a61caf328258be20f106f2b264ba0fdd17da
marianterinte <marian.terinte@gmail.com>
2021-05-31 22:07:56 +0300

    3. Added other phone.

--- SummerSchool2021/SummerSchool2021/Phone.cs
+++ SummerSchool2021/SummerSchool2021/Phone.cs
@@ -7,12 +7,14 @@ namespace SummerSchool2021
         private string make;
         private string model;
         private string display;
+        private string battery;
 
-        public Phone(string make, string model, string display)
+        public Phone(string make, string model, string display, string battery)
         {
             this.make = make;
             this.model = model;
             this.display = display;
+            this.battery = battery;
         }
 
         internal void Display()
@@ -20,9 +22,10 @@ namespace SummerSchool2021
             Console.WriteLine("-------------------------------------------------------");
             Console.WriteLine("Phone Specs");
             Console.WriteLine("-------------------------------------------------------");
-            Console.WriteLine(this.make);
-            Console.WriteLine(this.model);
+            Console.WriteLine(this.make+ " "+ this.model);
             Console.WriteLine(this.display);
+            Console.WriteLine(this.battery);
+
         }
     }
 }
--- SummerSchool2021/SummerSchool2021/Program.cs
+++ SummerSchool2021/SummerSchool2021/Program.cs
@@ -11,7 +11,12 @@ namespace SummerSchool2021
 
             if (name == "Iphone8")
             {
-                var phone = new Phone("Iphone", "8", " display retina 6 inch");
+                var phone = new Phone("Iphone", "8", " display retina 6 inch", "2400mh");
+                phone.Display();
+            }
+            else if (name == "S10")
+            {
+                var phone = new Phone("Samsung", "10", " display 6 inch", "2500mh");
                 phone.Display();
             }
             else
