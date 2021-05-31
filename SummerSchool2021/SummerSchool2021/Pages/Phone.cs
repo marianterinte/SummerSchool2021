3f7affc79389230e11c9f3eaed10b48962a95d4c
Marian Terinte <marian.terinte2@ucb.com>
2021-05-31 11:03:31 +0300

    1. List of Phones.

---
+++ SummerSchool2021/SummerSchool2021/Pages/Phone.cs
@@ -0,0 +1,23 @@
+﻿namespace SummerSchool2021.Pages
+{
+    public class Phone
+    {
+        private string make;
+        private string model;
+        private string color;
+        private string battery;
+
+        public Phone(string make, string model, string color, string battery)
+        {
+            this.make = make;
+            this.model = model;
+            this.color = color;
+            this.battery = battery;
+        }
+
+        public string Make { get => make; set => make = value; }
+        public string Model { get => model; set => model = value; }
+        public string Color { get => color; set => color = value; }
+        public string Battery { get => battery; set => battery = value; }
+    }
+}
