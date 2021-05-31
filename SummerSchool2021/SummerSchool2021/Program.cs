7bc430361c988ff3acf744d87f95bb58d327598f
marianterinte <marian.terinte@gmail.com>
2021-05-31 22:10:41 +0300

    4. Switch

--- SummerSchool2021/SummerSchool2021/Program.cs
+++ SummerSchool2021/SummerSchool2021/Program.cs
@@ -9,19 +9,26 @@ namespace SummerSchool2021
             Console.WriteLine("Phone Name:");
             var name = Console.ReadLine();
 
-            if (name == "Iphone8")
+            switch (name)
             {
-                var phone = new Phone("Iphone", "8", " display retina 6 inch", "2400mh");
-                phone.Display();
-            }
-            else if (name == "S10")
-            {
-                var phone = new Phone("Samsung", "10", " display 6 inch", "2500mh");
-                phone.Display();
-            }
-            else
-            {
-                Console.WriteLine("Phone not found");
+                case "Iphone8":
+                    {
+                        var phone = new Phone("Iphone", "8", " display retina 6 inch", "2400mh");
+                        phone.Display();
+                        break;
+
+                    }
+                case "S10":
+                    {
+                        var phone = new Phone("Samsung", "10", " display 6 inch", "2500mh");
+                        phone.Display();
+                        break;
+                    }
+                default:
+                    {
+                        Console.WriteLine("Phone not found");
+                        break;
+                    }
             }
         }
     }
