9b7790bd5dd4745d6ea14801b25c14a5d031e5d4
Marian Terinte <marian.terinte2@ucb.com>
2021-05-31 17:23:35 +0300

    1. Phone class.

---
+++ SummerSchool2021/SummerSchool2021/Phone.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace SummerSchool2021
+{
+    internal class Phone
+    {
+        private string make;
+        private string model;
+        private string display;
+
+        public Phone(string make, string model, string display)
+        {
+            this.make = make;
+            this.model = model;
+            this.display = display;
+        }
+
+        internal void Display()
+        {
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Phone Specs");
+            Console.WriteLine("-------------------------------------------------------");
+            Console.WriteLine("Iphone 8");
+            Console.WriteLine("Processor: xxxx");
+            Console.WriteLine("Display 8 inch");
+        }
+    }
+}
--- SummerSchool2021/SummerSchool2021/Program.cs
+++ SummerSchool2021/SummerSchool2021/Program.cs
@@ -1,26 +1,23 @@
-using Microsoft.AspNetCore.Hosting;
-using Microsoft.Extensions.Configuration;
-using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+﻿using System;
 
 namespace SummerSchool2021
 {
     public class Program
     {
-        public static void Main(string[] args)
+        static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
-        }
+            Console.WriteLine("Phone Name:");
+            var name = Console.ReadLine();
 
-        public static IHostBuilder CreateHostBuilder(string[] args) =>
-            Host.CreateDefaultBuilder(args)
-                .ConfigureWebHostDefaults(webBuilder =>
-                {
-                    webBuilder.UseStartup<Startup>();
-                });
+            if (name == "Iphone8")
+            {
+                var phone = new Phone("Iphone", "8", "");
+                phone.Display();
+            }
+            else
+            {
+                Console.WriteLine("Phone not found");
+            }
+        }
     }
 }
