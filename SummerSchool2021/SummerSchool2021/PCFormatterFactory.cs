2b02af6576b074209c23f5f29446e7d8ddf1747b
Marian Terinte <marian.terinte2@ucb.com>
2021-06-03 10:48:19 +0300

    Get ridd of the switch.

--- SummerSchool2021/SummerSchool2021/PCFormatterFactory.cs
+++ SummerSchool2021/SummerSchool2021/PCFormatterFactory.cs
@@ -1,10 +1,20 @@
 ﻿//1 Implemented
 //2 Implemented
+using System;
+using System.Collections.Generic;
+
 namespace SummerSchool2021
 {
     internal class PCFormatterFactory
     {
         private string display;
+
+        private Dictionary<string, IPCFormatter> formatterMap = new Dictionary<string, IPCFormatter>()
+        {
+            {"minimal", new MinimalPCFormatter()},
+            {"full", new MinimalPCFormatter()},
+            {"id", new MinimalPCFormatter()},
+        };
 
         public PCFormatterFactory(string display)
         {
@@ -13,15 +23,10 @@ namespace SummerSchool2021
 
         public IPCFormatter GetFormat()
         {
-            switch (display)
-            {
-                case "minimal":
-                    return new MinimalPCFormatter();
-                case "full":
-                    return new FullPCFormatter();
-                default:
-                    return new IdPCFormatter();
-            }
+            if (formatterMap.ContainsKey(display))
+                return formatterMap[display];
+
+            throw new Exception("No format found");
         }
     }
 }
